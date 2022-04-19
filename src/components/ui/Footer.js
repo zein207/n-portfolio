86d8e85158afd7a4b7d6fd9c747082e07b927437
David <zein207@gmail.com>
2022-04-19 12:41:58 -0500

    Footer [Mobile styles]

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
 export const Footer = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='footer__main-container'>
+
+        <div className='footer__legal'>
+          <p>David Ponce &copy; { currentYear }</p>
+        </div>
+
+        <div className='footer__social'>
+          <ul className='footer__social-list'>
+
+            <li>
+              <a href='https://www.linkedin.com/in/david-ponce-681450204' target='_blank'><i class="fa-brands fa-linkedin-in"></i></a>
+            </li>
+
+            <li>
+              <a href='https://github.com/zein207' target='_blank'><i class="fa-brands fa-github"></i></a>
+            </li>
+
+            <li>
+              <a href='https://www.instagram.com/david_ponce.e' target='_blank'><i class="fa-brands fa-instagram"></i></a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </footer>
   )
 }
