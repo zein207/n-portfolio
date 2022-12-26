37d4d32365efe10e1892115dc0d1bb1a9a5b31e4
David Ponce <zein207@gmail.com>
2022-12-25 22:32:28 -0600

    Update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,11 +7,88 @@ export const projects = [
         'url': 'https://www.ae.com.gt/'
     },
     {
+        'id': 'taf',
+        'name':'TAF MX', 
+        'cover':'images/projects/taf-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.taf.com.mx/'
+    },
+    {
+        'id': 'calvin-klein',
+        'name':'Calvin Klein GT', 
+        'cover':'images/projects/calvin-klein-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.calvinklein.gt/'
+    },
+    {
+        'id': 'victorias-secret-ar',
+        'name':'Victoria`s Secret Beauty Argentina', 
+        'cover':'images/projects/vsa-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.victoriassecretbeauty.ar/'
+    },
+    {
+        'id': 'ferretop',
+        'name':'Ferretop', 
+        'cover':'images/projects/ferretop-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.ferretop.com.mx/'
+    },
+    {
+        'id': 'baby-mink',
+        'name':'Baby Mink', 
+        'cover':'images/projects/baby-mink-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.babymink.com.mx/'
+    },
+    {
+        'id': 'vinoteca',
+        'name':'Vinoteca', 
+        'cover':'images/projects/vinoteca-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.vinoteca.com/'
+    },
+    {
+        'id': 'tommy-hilfiger',
+        'name':'Tommy Hilfiger MX', 
+        'cover':'images/projects/tommy-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://mx.tommy.com/'
+    },
+    {
+        'id': 'payless',
+        'name':'Payless', 
+        'cover':'images/projects/payless-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://ecuador.payless.com/'
+    },
+    {
+        'id': 'office-max',
+        'name':'Office Max', 
+        'cover':'images/projects/office-max-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.officemax.com.mx/'
+    },
+    {
         'id': 'distroller',
         'name':'Distroller', 
         'cover':'images/projects/distroller-cover.jpg', 
         'project_description': 'eCommerce implementation with VTEX & VTEX IO',
-        'url': 'https://mexico.distroller.com/'
+        'url': 'https://espana.distroller.com/'
+    },
+    {
+        'id': 'party-city',
+        'name':'Party City MX', 
+        'cover':'images/projects/party-city-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.partycity.com.mx/'
+    },
+    {
+        'id': 'bodegas-alianza',
+        'name':'Bodegas Alianza', 
+        'cover':'images/projects/bodegas-cover.jpg', 
+        'project_description': 'eCommerce implementation with VTEX & VTEX IO',
+        'url': 'https://www.bodegasalianza.com/'
     },
     {
         'id': 'e-ecommerce',
@@ -21,24 +98,10 @@ export const projects = [
         'url': 'https://e-ecommerce-rho.vercel.app'
     },
     {
-        'id': 'journal-app',
-        'name':'Journal App', 
-        'cover':'images/projects/journal-app-cover.jpg', 
-        'project_description': 'Application that allows the creation of personal notes',
-        'url': 'https://journal-app-gray.vercel.app/'
-    },
-    {
         'id': 'plants-shop',
         'name':'Bonsai Plants Shop', 
         'cover':'images/projects/plants-cover.jpg', 
         'project_description': 'Landing page of a plants shop',
         'url': 'https://zein207.github.io/bonsai/'
-    },
-    {
-        'id': 'calendar-app',
-        'name':'Calendar App', 
-        'cover':'images/projects/calendar-cover.jpg', 
-        'project_description': 'Calendar application',
-        'url': 'https://mern-calendar-david-p.herokuapp.com/'
     }
 ]
